af996c7022fdd037db08c9139137bc9b4e6d80a0
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-03-04 19:36:04 -0300

    feat:Movie model refactor

--- MoviesAPI/src/main/java/net/weg/moviesapi/model/entity/Movie.java
+++ MoviesAPI/src/main/java/net/weg/moviesapi/model/entity/Movie.java
@@ -22,7 +22,7 @@ public class Movie {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
 
     @Column(nullable = false)
